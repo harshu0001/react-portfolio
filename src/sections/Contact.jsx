6639cd98167188b5b167f438ed66487b574e1168
harshu0001 <prataph229@gmail.com>
2024-12-01 22:15:39 +0530

    v-1

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: 'JavaScript Mastery',
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
+          to_email: 'hpsingh8630@gmail.com',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -9,17 +9,17 @@ const Footer = () => {
 
       <div className="flex gap-3">
         <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          <a href="https://github.com/harshu0001"><img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" /></a>
         </div>
-        <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
+        {/* <div className="social-icon">
+          <a><img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" /></a>
+        </div> */}
+        {/* <div className="social-icon">
+          <a><img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" /></a>
+        </div> */}
       </div>
 
-      <p className="text-white-500">© 2024 Adrian Hajdin. All rights reserved.</p>
+      <p className="text-white-500">© 2024 Harshu ❤️ All rights reserved.</p>
     </footer>
   );
 };
